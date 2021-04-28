8c11ee4afc2289505a4aef6d6a95f48fd312dcf7
TasneemMohammad <tasneemmohamed504@gmail.com>
2021-04-28 09:26:49 +0200

    Activity_main_cards is added

--- app/src/main/java/com/example/qualificationmarrigeproject/MainActivity.java
+++ app/src/main/java/com/example/qualificationmarrigeproject/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.qualificationmarrigeproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.qualificationmarrigeproject.Activty_main_Cards.Cards_Activty;
+
+//login
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_login ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_login = findViewById(R.id.btn_login);
+      btn_login.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              Intent intent = new Intent(MainActivity.this , Cards_Activty.class);
+              startActivity(intent);
+          }
+      });
+
     }
 }
